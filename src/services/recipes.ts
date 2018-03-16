8ac3d31b33602b6a505d02ae874417b8c0982e3a
YiZhang <hyacinthzy1@gmail.com>
2018-03-16 13:08:15 +1100

    popover bug fixing

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -51,7 +51,13 @@ export class RecipesService {
     return this.http.get('https://ionic2-recipebook-f22d3.firebaseio.com/' +
       userId + '/recipes.json?auth=' + token)
       .map((response: Response) => {
-        return response.json();
+        const recipes: Recipe[] = response.json() ? response.json() : [];
+        for (let item of recipes) {
+          if (!item.hasOwnProperty('ingredients')) {
+            item.ingredients = [];
+          }
+        }
+        return recipes;
       })
       .do((recipes: Recipe[]) => {
         if (recipes) {
